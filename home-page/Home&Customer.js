85b664d79102a8afd89144163da9c94754d17b11
Hassaan-here <hassaanmuneer854@gmail.com>
2023-07-18 16:49:56 +0500

    Updated

--- home-page/Home&Customer.js
+++ home-page/Home&Customer.js
@@ -29,13 +29,13 @@ function toggleNav() {
 }
 
 // drop down function
-var dropdown = document.getElementsByClassName("dropdown");
-var i;
+let dropdown = document.getElementsByClassName("dropdown");
+let i;
 
 for (i = 0; i < dropdown.length; i++) {
   dropdown[i].addEventListener("click", function () {
     this.classList.toggle("active");
-    var dropdownContent = this.nextElementSibling;
+    let dropdownContent = this.nextElementSibling;
     if (dropdownContent.style.display === "block") {
       dropdownContent.style.display = "none";
     } else {
@@ -46,3 +46,39 @@ for (i = 0; i < dropdown.length; i++) {
 
 // Call the function when the window is resized
 window.addEventListener("resize", applyCodeBasedOnScreenWidth);
+
+// for hidden section of Modal
+// to make it visible on click
+function viewmore() {
+  let hiddenElement = document.getElementById("hidden");
+  if (hiddenElement.style.display === "block") {
+    hiddenElement.style.display = "none";
+  } else {
+    hiddenElement.style.display = "block";
+  }
+}
+
+function view1() {
+  let individualSection = document.getElementById("individual-section");
+  let businessSection = document.getElementById("business-section");
+
+  if (individualSection.style.display === "block") {
+    individualSection.style.display = "none";
+  } else {
+    individualSection.style.display = "flex";
+    businessSection.style.display = "none";
+  }
+}
+
+function view2() {
+  let individualSection = document.getElementById("individual-section");
+  let businessSection = document.getElementById("business-section");
+  // for business
+
+  if (businessSection.style.display === "block") {
+    businessSection.style.display = "none";
+  } else {
+    businessSection.style.display = "flex";
+    individualSection.style.display = "none";
+  }
+}
